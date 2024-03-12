cf9fd21e9dbe5e0a7e7b8d22835f5ed177b3797e
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-03-12 22:07:30 +0530

    Treasurehunt improved

--- app/treasure/page.js
+++ app/treasure/page.js
@@ -3,40 +3,47 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Hunt1 = () => {
-  const [answer1, setAnswer1] = useState("");
-  const [team, setTeam] = useState(null);
+	const [answer1, setAnswer1] = useState("");
+	const [team, setTeam] = useState(null);
 
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    const teamData = JSON.parse(localStorage.getItem("team")) || null;
-    if (teamData == null || teamData?.hunt8 === undefined) {
-      router.push("/hunt8");
-    } else {
-    }
+	useEffect(() => {
+		const teamData = JSON.parse(localStorage.getItem("team")) || null;
+		if (teamData == null || teamData?.hunt8 === undefined) {
+			router.push("/hunt8");
+		} else {
+		}
 
-    setTeam(JSON.parse(localStorage.getItem("team")));
-  }, []);
+		setTeam(JSON.parse(localStorage.getItem("team")));
+	}, []);
 
-  return (
-    <>
-      <div className="md:hidden border-white border-2 mx-8 my-8  rounded-lg shadow-md p-4 text-center">
-        <p>Mobile pe mat dekho sir/mam. PC pe check karo pleaseeee</p>
-      </div>
+	return (
+		<>
+			<div className="md:hidden border-white border-2 mx-8 my-8  rounded-lg shadow-md p-4 text-center">
+				<p>Mobile pe mat dekho sir/mam. PC pe check karo pleaseeee</p>
+			</div>
 
-      <div className="hidden md:flex bg-[url('../public/landing.png')] bg-cover text-gray-300 min-h-screen  justify-center items-center">
-        <div className="bg-[#131313] text-[#FFD700]  bg-[url('../public/shapess.png')] bg-cover p-8 rounded-2xl shadow-md w-[1278px] h-[685px] flex flex-row justify-around items-center object-cover ">
-          <div className="flex flex-col gap-4">
-            <h4 className="text-white text-xl ">{`Congratulations team ${team?.teamName},you have completed the quest.`}</h4>
-            <h5 className="text-4xl text-center font-bold">AABB9900123TG</h5>
-            <h4 className="text-white text-center text-xl ">
-              Run and write this code on white board
-            </h4>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+			<div className="hidden md:flex bg-[url('../public/landing.png')] bg-cover text-gray-300 min-h-screen  justify-center items-center">
+				<div className="bg-[#131313] text-[#FFD700]  bg-[url('../public/shapess.png')] bg-cover p-8 rounded-2xl shadow-md w-[1278px] h-[685px] flex flex-row justify-around items-center object-cover ">
+					<div className="flex flex-col gap-4 space-y-10">
+						<h4 className="text-white text-3xl font-black mx-auto ">
+							Congratulations team{" "}
+							<span className="bg-[#7f00ff] p-2 rounded-xl">
+								AlphaCoders {team?.teamName}
+							</span>
+						</h4>
+						<h5 className="text-8xl text-center font-bold">
+							AABB9900123TG
+						</h5>
+						<h4 className="text-white font-thin text-center text-3xl italic">
+							🏃🏻 Run and write this code on white board 🏃🏿
+						</h4>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Hunt1;
